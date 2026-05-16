1abc1f017a6ea73b32339e233549073279dbad1a
Breckfab <151375745+Breckfab@users.noreply.github.com>
2026-05-16 09:29:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ st.set_page_config(page_title="PsicoNexo", page_icon="🧠", layout="centered")
 
 init_db()
 
-
 if "usuario" not in st.session_state:
     st.session_state.usuario = None
 if "pagina" not in st.session_state:
     st.session_state.pagina = "login"
 
 def mostrar_login():
-    st.title("🧠 PsicoNexo")
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2:
+        st.image("PsicoNexo_png.png", use_container_width=True)
     st.subheader("Iniciá sesión")
     with st.form("form_login"):
         email = st.text_input("Email")
@@ -33,7 +34,9 @@ def mostrar_login():
         st.rerun()
 
 def mostrar_registro():
-    st.title("🧠 PsicoNexo")
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2:
+        st.image("PsicoNexo_png.png", use_container_width=True)
     st.subheader("Crear cuenta")
     carreras = get_carreras()
     opciones = {f"{c[1]} — {c[2]}": c[0] for c in carreras}
@@ -67,9 +70,9 @@ def mostrar_app():
     usuario = st.session_state.usuario
 
     with st.sidebar:
+        st.image("PsicoNexo_png.png", use_container_width=True)
         st.markdown(f"### 👤 {usuario['nombre']}")
         st.markdown("---")
-        st.markdown("📋 **Menú**")
         if st.button("🏠 Inicio"):
             st.session_state.pagina = "home"
             st.rerun()
@@ -80,8 +83,6 @@ def mostrar_app():
         if st.button("Cerrar sesión"):
             logout()
             st.rerun()
-
-    st.write(f"DEBUG carrera_id: {usuario['carrera_id']}")
 
     if st.session_state.pagina == "materias":
         from pages import materias
